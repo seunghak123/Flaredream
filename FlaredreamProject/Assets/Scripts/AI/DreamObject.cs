42245da8cce0add62e93e6f1a5c80ffa12e71c51
seunghak123 <38850267+seunghak123@users.noreply.github.com>
2021-04-25 23:17:18 +0900

    dd
    dd

---
+++ FlaredreamProject/Assets/Scripts/AI/DreamObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DreamObject : BaseObject
+{
+    public BaseAI m_baseAI = null;
+
+    [SerializeField]
+    GameObject m_PrefabShadowObject = null;
+
+    [NonSerialized]
+    GameObject m_ShadowObject = null;
+
+    public void InitObject()
+    {
+        if (m_PrefabShadowObject != null)
+        {
+            m_ShadowObject = Instantiate(m_PrefabShadowObject) as GameObject;
+            if (m_ShadowObject != null)
+            {
+                m_ShadowObject.transform.SetParent(SelfTransform, false);
+            }
+        }
+    }
+}
---
+++ FlaredreamProject/Assets/Scripts/AI/PlayerAI.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerAI : BaseAI
+{
+    protected bool m_PauseUpdate = false;
+    [SerializeField]
+    NavMeshAgent m_navigate = null;
+    public void Update()
+    {
+        Inputdirect = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+    }
+    public override void InitAI()
+    {
+        StartCoroutine(InitSuccess());
+    }
+    private Vector3 Inputdirect = Vector3.zero;
+    IEnumerator InitSuccess()
+    {
+        float fPrevTime = Time.time;
+        float fPrevUnscaleTime = Time.unscaledTime;
+        float fCurrentTime = 0;
+        float fCurrentUnscaleTime = 0;
+        while (true)
+        {
+            fCurrentTime = Time.time;
+            fCurrentUnscaleTime = Time.unscaledTime;
+
+            float deltaTime = fCurrentTime - fPrevTime;
+            float unscaleDeltaTime = fCurrentUnscaleTime - fPrevUnscaleTime;
+
+            fPrevTime = fCurrentTime;
+            fPrevUnscaleTime = fCurrentUnscaleTime;
+
+            while (m_PauseUpdate == true)
+            {
+                yield return null;
+            }
+
+
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+    protected override void ProcessMove()
+    {
+        if(movedirect!= Vector3.zero)
+        {
+            Vector3 m_movePosition = SelfTransform.position + (Inputdirect * Time.deltaTime);
+            m_navigate.Move(m_movePosition);//MoveDestination(m_movePosition);
+            m_navigate.isStopped = false;
+            //StopDestination(false);
+        }
+        else
+        {
+            NextAI(e_State.Idle);
+        }
+    }
+    protected override void ProcessDie()
+    {
+
+    }
+    protected override void ProcessInterAct()
+    {
+
+    }
+}
